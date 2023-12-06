e4e78b2513244805e31d2494626c43df9cad0ae5
Mark <Aizeklol50@gmail.com>
2023-12-06 22:16:02 +0700

    Добавлен Fragment внутрь MainActivity.kt
    Создан фрагмент my_fragment.xml.
    
    Создан MyFragment.kt для дальнейшей реализации.

--- app/src/main/java/com/kolesovmark/markcounties/MainActivity.kt
+++ app/src/main/java/com/kolesovmark/markcounties/MainActivity.kt
@@ -3,7 +3,10 @@ package com.kolesovmark.markcounties
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.kolesovmark.markcounties.databinding.ActivityMainBinding
+import com.kolesovmark.markcounties.databinding.MyFragmentBinding
 import kotlin.time.Duration.Companion.seconds
 
 // показать, сколько секунд приложение было свернуто
@@ -13,36 +16,49 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var timeOnStop: Long = 0
     private var timeOnRestart: Long = 0
+
     //private var timer = Timer(1000000, 1000)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        binding.buttonToast.setOnClickListener {
-            Toast.makeText(
-                this,
-                "Clicked",
-                Toast.LENGTH_LONG).show()
-        }
-        binding.radioToast.setOnCheckedChangeListener { compoundButton, b ->
-            if (b) {
-                Toast.makeText(
-                    this,
-                    "Clicked Radio",
-                    Toast.LENGTH_LONG).show()
-            }
-        }
-        binding.switchToast.setOnCheckedChangeListener { compoundButton, b ->
-            if (b) {
-                Toast.makeText(
-                    this,
-                    "Clicked Switch",
-                    Toast.LENGTH_LONG).show()
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .commit {
+                setReorderingAllowed(true)
+                add<MyFragment>(R.id.fragment_container_view)
+                var fragmentBinding = MyFragmentBinding.inflate(layoutInflater)
+                fragmentBinding.buttonToast.setOnClickListener {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Clicked",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                fragmentBinding.radioToast.setOnCheckedChangeListener { compoundButton, b ->
+                    if (b) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Clicked Radio",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+                fragmentBinding.switchToast.setOnCheckedChangeListener { compoundButton, b ->
+                    if (b) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Clicked Switch",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
             }
         }
 
     }
+
     override fun onStop() {
         super.onStop()
         timeOnStop = System.nanoTime()
@@ -53,15 +69,8 @@ class MainActivity : AppCompatActivity() {
         timeOnRestart = System.nanoTime()
         Toast.makeText(
             this,
-            "${((timeOnRestart - timeOnStop)*1e-9)} секунд",
-            Toast.LENGTH_LONG).show()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Toast.makeText(
-            this,
-            "${((timeOnRestart - timeOnStop)*1e-9).seconds}",
-            Toast.LENGTH_LONG).show()
+            "${((timeOnRestart - timeOnStop) * 1e-9)} секунд",
+            Toast.LENGTH_LONG
+        ).show()
     }
 }
---
+++ app/src/main/java/com/kolesovmark/markcounties/MyFragment.kt
@@ -0,0 +1,6 @@
+package com.kolesovmark.markcounties
+
+import androidx.fragment.app.Fragment
+
+class MyFragment : Fragment(R.layout.my_fragment) {
+}
